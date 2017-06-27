984b6a67fa606385946b7ea14caf856ce567ca8e
Sergei Golovin <golovin@cs.karelia.ru>
2017-06-27 16:52:30 +0300

    client authorization check

--- client/controllers/mainController.js
+++ client/controllers/mainController.js
@@ -7,15 +7,47 @@ var app = angular.module('SEManager', ["ngRoute"]).controller('mainController',
 app.config(function($routeProvider){
     $routeProvider
     .when('/', {
-        templateUrl : "/templates/loginForm.htm"
+        templateUrl : "/templates/loginForm.htm",
+        controller: 'LoginCtrl'
     })
     .when('/securedPage', {
-        templateUrl : "/templates/securedPage.htm"
+        templateUrl : "/templates/securedPage.htm",
+        controller: 'securedPageCtrl'
     })
     .when('/page', {
-        templateUrl : "/templates/page.htm"
+        templateUrl : "/templates/page.htm",
+        controller: 'PageCtrl'
     })
     .when('/register',{
-        templateUrl : "/templates/signupForm.htm"
+        templateUrl : "/templates/signupForm.htm",
+        controller: 'SignInCtrl'
     })
 });
+
+app.controller('LoginCtrl', function($scope, $rootScope, $http) {
+
+});
+
+app.controller('securedPageCtrl', function($scope, $rootScope, $http, $location) {
+    $http.get("/currentUser").then(function(response) {
+	if(response.data === '')
+	    $location.url('/');
+	else
+	    $scope.user = response.data;
+    });
+});
+
+app.controller('PageCtrl', function($scope, $rootScope, $http, $location) {
+    $http.get("/currentUser").then(function(response) {
+	if(response.data === '')
+	    $location.url('/');
+	else
+	    $scope.user = response.data;
+	
+    });
+});
+
+app.controller('SignInCtrl', function($scope, $rootScope, $http, $location) {
+
+});
+
--- server/models/event.js
+++ server/models/event.js
@@ -1,3 +1,4 @@
+"use strict";
 var sqlite3 = require('sqlite3');
 var db = new sqlite3.Database('./users.db');
 
