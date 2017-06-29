7fd4396ce97a2fd3e56a840b0589f827b4d644bf
Sirozha1337 <schernyae@gmail.com>
2017-06-29 16:45:25 +0300

    Changes to classes, possible fix to users in event

--- server/models/event.js
+++ server/models/event.js
@@ -2,28 +2,23 @@
 var db = require('../config/database.js');
 var bcrypt = require('bcrypt-nodejs');
 var mail = require('../config/mail.js');
-// Establish database connection
+
+// Establish database connection and create tables if needed
 db.serialize(function() {
-  //  db.run("DROP TABLE events");
-  //  db.run("DROP TABLE invites");
     db.run("CREATE TABLE IF NOT EXISTS events (name TEXT NOT NULL, description TEXT NOT NULL, ownerId INTEGER NOT NULL, id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL)");
     db.run("CREATE TABLE IF NOT EXISTS invites (eventId INTEGER NOT NULL, userId INTEGER NULL, inviteCode TEXT NULL)");
 });
 
 var self = module.exports = {
-    Event: class{
-        constructor(name, description, ownerId){
-            this.name = name;
-            this.description = description;
-            this.ownerId = ownerId;
-        }
-
-        save(callback){
-            var event = this;
-            db.run("INSERT INTO events(name, description, ownerId) VALUES(?, ?, ?)", 
-                this.name, 
-                this.description, 
-                this.ownerId, 
+    addEvent: function(name, description, ownerId, callback){
+        var event = {};
+        event.name = name;
+        event.description = description;
+        event.ownerId = ownerId;
+        db.run("INSERT INTO events(name, description, ownerId) VALUES(?, ?, ?)", 
+                event.name, 
+                event.description, 
+                event.ownerId, 
                 function(err){
                     if(err){
                         console.log('Error adding new event: ' + err);
@@ -33,8 +28,7 @@ var self = module.exports = {
                         event.id = this.lastID;
                         callback(event);
                     }
-            });
-        }
+        });
     },
     findEventsByOwner: function (ownerId, callback){
         db.all("SELECT * from events WHERE ownerId = ?", ownerId, function(err, rows){
@@ -50,13 +44,13 @@ var self = module.exports = {
         });
     },
     findUsersByEvent: function (eventId, callback){
-        db.all("SELECT users.* from users INNER JOIN invites ON users.id=invites.userId WHERE invites.eventId=?", eventId, function(err, rows){
+        db.all("SELECT users.* from users INNER JOIN invites ON users.id=invites.userId WHERE eventId=?", eventId, function(err, rows){
             if(err){
-                console.log('Error finding users in this event'+err);
+                console.log('Error finding users in this event:'+err);
                 return callback(false);
             }
             if(rows.length == 0){
-                console.log('no events');
+                console.log('no users');
                 return callback(false);
             }
             return callback(rows);
