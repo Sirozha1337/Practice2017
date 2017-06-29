e0d4ac269aa8d884cfcbba98cfecce00d1857a2e
Sergei Golovin <golovin@cs.karelia.ru>
2017-06-29 16:05:23 +0300

    small alert fix

--- client/controllers/localLoginCtrl.js
+++ client/controllers/localLoginCtrl.js
@@ -1,20 +1,23 @@
 /* Controls local login page */
 app.controller('localLoginCtrl', function($scope, $http, $location){
-		$scope.localEmail = '';
-		$scope.localPass = '';
-		$scope.localLoginError = '';
-        $scope.localLogin = function(){
-			var formData = {};
-			formData.email = $scope.localEmail;
-			formData.password = $scope.localPass;
-			$http.post('/login', formData).then(function(response){
-                /* On success redirect to second page */
-				$location.url('/securedPage');
-			}).catch(function(response){
-                /* On error display error message */
-				$scope.localLoginError = response.data;
-			});
-		}
+    $scope.localEmail = '';
+    $scope.localPass = '';
+    $scope.localLoginError = '';
+    $scope.errid = 0;
+    
+    $scope.localLogin = function(){
+	var formData = {};
+	formData.email = $scope.localEmail;
+	formData.password = $scope.localPass;
+	$http.post('/login', formData).then(function(response){
+            /* On success redirect to second page */
+	    $location.url('/securedPage');
+	}).catch(function(response){
+            /* On error display error message */
+	    $scope.localLoginError = response.data;
+	    $scope.errid = 1;
+	});
     }
+}
 );
 
